35209ceb1ad9200d063b19ea2f11fd3191bebf30
Sunny Kumar Singh <srajukumarsingh@ivycomptech.com>
2024-11-17 17:35:30 +0530

    Added key in .env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyB6TC5yfQnd01mKQSem_2qdnau5ej35U8g";
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
